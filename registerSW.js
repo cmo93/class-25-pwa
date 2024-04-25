227e850731431ec0c202b7baf3b9055efce61559
cmo93 <90342386+cmo93@users.noreply.github.com>
2024-04-25 15:16:31 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ctrl-alt-elite/sw.js', { scope: '/ctrl-alt-elite/' })})}
