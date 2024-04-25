792e2991b44e7c3c64356375a03d95f7eb8c697b
cmo93 <90342386+cmo93@users.noreply.github.com>
2024-04-25 15:26:24 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ctrl-alt-elite-chiweta/sw.js', { scope: '/ctrl-alt-elite-chiweta/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/class-25-pwa/sw.js', { scope: '/class-25-pwa/' })})}
